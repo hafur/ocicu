647f759dd17a507835b8f6e100e61457b6b57998
klazarz <klazarz@KLAZARZ-NL.nl.oracle.com>
2018-10-30 16:23:37 +0100

    update code base

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ import oci_setup as ocis
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username':'kevin'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username':'Kevin'}
+    list_services = {'services': ocis.print_resources()}
+    return render_template('index.html', title='Home', user=user, list_services=list_services)
 
--- oci_setup.py
+++ oci_setup.py
@@ -1,21 +1,31 @@
 import oci
 """This is a doc string test"""
 
-
 def print_resources():
     """This is is an example docstring"""
-    config = oci.config.from_file("~/dev/ocicu/.oci/config", "DEFAULT")
-    identity = oci.identity.IdentityClient(config)
-    user = identity.get_user(config["user"]).data
-    print(user)
+    config = oci.config.from_file("/mnt/c/Users/klazarz/Desktop/dev/ocicu/.oci/config", "DEFAULT")
+    #identity = oci.identity.IdentityClient(config)
+    #user = identity.get_user(config["user"]).data
+    #print(user)
     resources_tenancy = oci.resource_search.ResourceSearchClient(config)
     resources = resources_tenancy.list_resource_types()
     data = resources.data
-    # print(resources.data)
+    #print(resources.data)
+    list_services = []
+    i = 0
     for key in data:
-        print(key.name)
-        if key.name == 'AutonomousDataWarehouse':
-            print(key.fields[1])
+        print (i)
+        
+        list_services.append(key.name)
+        #if key.name == 'AutonomousDataWarehouse':
+         #   list_services.append(key.fields[1])    
+        #    print(key.fields[1])
 
+    #print(list_services)
 
-print_resources()
+    for service, j in enumerate(list_services):
+        list_all.append(service[j])
+
+    print(len(list_services))
+    return list_services
+
